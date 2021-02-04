650d5c91273613b318c44d97a5f53a495a71683c
Akhlaque <akhlaquea01@gmail.com>
2021-02-04 17:12:05 +0530

    Assignment 2 employee component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './components/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { EmployeeComponent } from './components/employee/employee.component';
 @NgModule({
-  declarations:[AppComponent,HeaderComponent, FooterComponent],
+  declarations:[AppComponent,HeaderComponent, FooterComponent, EmployeeComponent],
   imports:[BrowserModule],
   providers:[],
   bootstrap:[AppComponent]
